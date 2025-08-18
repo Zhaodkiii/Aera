3174ec9934ce663ccf7b1e6bff03b691ed056d69
Zhaodk <97621528@qq.com>
2025-08-18 20:43:12 +0800

    Initial Commit

---
+++ Aera/AeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeraApp.swift
+//  Aera
+//
+//  Created by Dream 話 on 2025/8/18.
+//
+
+import SwiftUI
+
+@main
+struct AeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
